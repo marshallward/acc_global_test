2ad16c4272bc61af62a6e8f229728f373fce33f9
Marshall Ward <marshall.ward@noaa.gov>
2024-08-02 13:33:36 -0400

    Manual deepcopy in main()
    Explicitly copying G%Idx and G%Idx appears to allow me to use
    
        !$acc kernels present(G)
    
    in the loop declaration.

--- loop.f90
+++ loop.f90
@@ -17,12 +17,12 @@ subroutine loop_with_g(G, field)
   !!! This alone does not work
   !!!$acc kernels
 
-  !!! ACC cannot find G
-  !!!$acc kernels present(G)
+  !!! This works, but copies G on every function call
+  !!!$acc enter data copyin(G)
+  !!!$acc kernels
 
-  ! This works, but copies G on every function call
-  !$acc enter data copyin(G)
-  !$acc kernels
+  ! This works, if I copy both G and its contents outside of the function
+  !$acc kernels present(G)
   do j = 1, G%nj
     do i = 1, G%ni
       field(i,j) = field(i,j) + (i - 1) * (j - 1) * G%Idx(i,j) * G%Idy(i,j)
--- main.f90
+++ main.f90
@@ -7,6 +7,9 @@ type(grid) :: G
 real, allocatable :: field(:,:)
 
 call create_grid(G, 64, 128)
+!$acc enter data copyin(G)
+!$acc enter data copyin(G%Idx)
+!$acc enter data copyin(G%Idy)
 
 allocate(field(G%ni, G%nj))
 
