94a13406ecb4223654da3f6068ca059690fae6ec
Marshall Ward <marshall.ward@noaa.gov>
2024-08-14 20:35:05 -0400

    Move G_in into a derived type (control struct)

--- main.f90
+++ main.f90
@@ -3,19 +3,23 @@ use loop_mod, only : loop_with_g
 
 implicit none
 
-type(grid), target :: G_in
+type :: main_cs
+    type(grid) :: G
+end type main_cs
+
+type(main_cs), target :: CS
 type(grid), pointer :: G
 real, allocatable :: field(:,:)
 
-call create_grid(G_in, 64, 128)
-allocate(field(G_in%ni, G_in%nj))
+call create_grid(CS%G, 64, 128)
+allocate(field(CS%G%ni, CS%G%nj))
 
-G => G_in
+G => CS%G
 
 ! NOTE: Both G and G_in appear to work here
-!$acc enter data copyin(G_in)
-!$acc enter data copyin(G_in%Idx)
-!$acc enter data copyin(G_in%Idy)
+!$acc enter data copyin(CS%G)
+!$acc enter data copyin(CS%G%Idx)
+!$acc enter data copyin(CS%G%Idy)
 call loop_with_g(G, field)
 
 print *, sum(field)
